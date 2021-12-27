0a1abd7994d9f7f08f43bf26346e18fe8d5ccf52
Çağdaş Aldoğan <cagdas.aldogan00@gmail.com>
2021-12-27 16:40:54 +0300

    first commit

---
+++ 1-Kvp/setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Memati Baş',
+    age: 40,
+    image:
+      'https://upload.wikimedia.org/wikipedia/tr/8/83/Memati_Kurtlar_vadisi_pusu.jpg',
+  },
+  {
+    id: 2,
+    name: 'Polat Alemdar',
+    age: 43,
+    image:
+      'https://scontent.fist13-1.fna.fbcdn.net/v/t1.18169-9/27545430_1581973025232672_8793593198263148728_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=aztMOTG9Y_kAX8eVWJp&_nc_ht=scontent.fist13-1.fna&oh=00_AT-Y8OgmfA4ynlhC7U5Z_GprInQt2qHo0gUwioCQ2sTQUA&oe=61EF66E4',
+  },
+  {
+    id: 3,
+    name: 'Süleyman Çakır',
+    age: 45,
+    image:
+      'https://im.haberturk.com/2021/04/08/ver1617870811/3032617_810x458.jpg',
+  },
+  {
+    id: 4,
+    name: 'Abdulhey ',
+    age: 39,
+    image:
+      'https://foto.haberler.com/haber/2014/07/29/kurtlar-vadisi-nde-abdulhey-diziden-ayrildi-6316077_2597_amp.jpg',
+  },
+  {
+    id: 5,
+    name: 'Halo Dayı',
+    age: 63,
+    image:
+      'https://img.piri.net/mnresize/840/-/resim/site/ex/halodayi007560e600739977by.jpg',
+  },
+];
